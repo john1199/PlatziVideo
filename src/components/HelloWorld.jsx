8d5abefbe1c72c9543f59b66e553a0621f47f898
john1199 <jhon091199@gmail.com>
2020-08-10 00:37:13 -0500

    ESLint y  configuracion completa espacio de trabajo

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../assets/styles/App.scss';//para que los estilos sea "agregados"
+
+const HelloWorld = () => (
+    <h1>Hola Mundo</h1>
+);
+
+export default HelloWorld;
