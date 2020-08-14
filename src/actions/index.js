ebfe534bed6a33f7002a3f8d4330cf3c1b4beb1b
john1199 <jhon091199@gmail.com>
2020-08-13 23:58:06 -0500

    Terminando reducer y boton eliminar favoritos

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,11 @@
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable no-multi-assign */
-export const setFavorite = payload => ({
+export const setFavorite = (payload) => ({
   type: 'SET_FAVORITE',
+  payload,
+});
+
+export const deleteFavorite = (payload) => ({
+  type: 'DELETE_FAVORITE',
   payload,
 });
