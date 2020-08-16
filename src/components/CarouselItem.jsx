9e8aa14dc0481b17f4a34b60495db9abfbf035ed
john1199 <jhon091199@gmail.com>
2020-08-15 21:14:30 -0500

    terminando player

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { setFavorite, deleteFavorite } from '../actions/index';
 
 import '../assets/styles/components/CarouselItem.scss';
@@ -24,11 +25,14 @@ const CarouselItem = (props) => {
       <img className='carousel-item__img' src={cover} alt={title} />
       <div className='carousel-item__details'>
         <div>
-          <img
-            className='carousel-item__details--img'
-            src={playIcon}
-            alt='Play Icon'
-          />
+          {/*nois permite que ir a diferentes url o algo asi :v */}
+          <Link to={`/player/${id}`}>
+            <img
+              className='carousel-item__details--img'
+              src={playIcon}
+              alt='Play Icon'
+            />
+          </Link>
           {isList ? (
             <img
               className='carousel-item__details--img'
---
+++ src/containers/Player.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useReducer } from 'react';
+import { connect } from 'react-redux';
+//import { Redirect } from 'react-router-dom';
+import NotFound from './NotFound';
+import { getVideoSource } from '../actions/index';
+import '../assets/styles/components/Player.scss';
+
+const Player = (props) => {
+  const { id } = props.match.params;
+  const hasPlaying = Object.keys(props.playing).length > 0;
+  //obtenemos el id 
+  useEffect(() => {
+    props.getVideoSource(id);
+  }, []);
+
+  return hasPlaying ? (
+    <div className='Player'>
+      <video controls autoPlay>
+        <source src={props.playing.source} type='video/mp4' />
+      </video>
+      <div className='Player-back'>
+        {/* funcion disponible al encapsular nuestra app en browser-loader */}
+        <button type='button' onClick={() => props.history.goBack()}>
+          Regresar
+        </button>
+      </div>
+    </div>
+  ) :
+    (
+      <NotFound />
+      /*<Redirect to='/404/' />*/
+    );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    playing: state.playing,
+  };
+};
+const mapDispatchToProps = {
+  getVideoSource,
+};
+//export default Player;
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
